e62200a3c87be269ea2b102204d2144151607c41
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-04 20:28:04 +0600

    set meaningfull name of variables and functions

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
-document.getElementById("generate-pin-btn").addEventListener("click",function(){
-   newValue("left-text-area");
+document.getElementById("generate-pin-btn").addEventListener("click", function() {
+	generateRandomDigit("left-text-area");
 })
 
-function newValue(id){
-    let radomDigit =  Math.floor(1000 + Math.random() * 9000);
-    let value = document.getElementById(id).value = radomDigit;
+function generateRandomDigit(id) {
+	let radomDigit = Math.floor(1000 + Math.random() * 9000);
+	document.getElementById(id).value = radomDigit;
 }
 
-function insert(val){
-    document.getElementById("right-text-area").value += val;
+function insert(value) {
+	document.getElementById("right-text-area").value += value;
 }
-
-document.getElementById("submitBtn").addEventListener("click",function(){
-   let value = document.getElementById("right-text-area").value;
-   if(value.length>0){
-        cheakValue();
-   }
-    
-    ClearFields();
+document.getElementById("submitBtn").addEventListener("click", function() {
+	let value = document.getElementById("right-text-area").value;
+	if(value.length > 0) {
+		cheakValueMatch();
+	}
+	clearFields();
 })
 
-function cheakValue(){
-    document.getElementById("match").style.display = "none";
-    document.getElementById("unMatch").style.display = "none";
-    let leftValue = document.getElementById("left-text-area").value;
-    let rightValue = document.getElementById("right-text-area").value;
-    if(leftValue==rightValue){
-        document.getElementById("match").style.display = "block";
-    }
-    else{
-        document.getElementById("unMatch").style.display = "block";
-        decrementValue();
-    }
+function cheakValueMatch() {
+	document.getElementById("match").style.display = "none";
+	document.getElementById("unMatch").style.display = "none";
+	let leftAreaValue = document.getElementById("left-text-area").value;
+	let rightAreaValue = document.getElementById("right-text-area").value;
+	if(leftAreaValue == rightAreaValue) {
+		document.getElementById("match").style.display = "block";
+	} else {
+		document.getElementById("unMatch").style.display = "block";
+		decrementValue();
+	}
 }
 
-function ClearFields() {
-    document.getElementById("right-text-area").value = "";
+function clearFields() {
+	document.getElementById("right-text-area").value = "";
 }
 
-function back(){
-    let value = document.getElementById("right-text-area").value;
-    let newValue = value.length -1;
-    document.getElementById("right-text-area").value = value.substring(0,newValue);    
+function back() {
+	let value = document.getElementById("right-text-area").value;
+	let newValue = value.length - 1;
+	document.getElementById("right-text-area").value = value.substring(0, newValue);
 }
 
-function decrementValue(){
-    let value = document.getElementById("try-value").innerText;
-    value--;
-    document.getElementById("try-value").innerText = value;
-    if(value == 0){
-        document.getElementById("submitBtn").disabled = true;
-    }
+function decrementValue() {
+	let value = document.getElementById("try-value").innerText;
+	value--;
+	document.getElementById("try-value").innerText = value;
+	if(value == 0) {
+		document.getElementById("submitBtn").disabled = true;
+	}
 }
