b0ad8353ee499de922c3da03c6768242e7adac43
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-04 12:27:17 +0600

    all is ok

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 document.getElementById("generate-pin-btn").addEventListener("click",function(){
    newValue("left-text-area");
 })
@@ -11,4 +9,46 @@ function newValue(id){
 
 function insert(val){
     document.getElementById("right-text-area").value += val;
+}
+
+document.getElementById("submitBtn").addEventListener("click",function(){
+   let value = document.getElementById("right-text-area").value;
+   if(value.length>0){
+        cheakValue();
+   }
+    
+    ClearFields();
+})
+
+function cheakValue(){
+    document.getElementById("match").style.display = "none";
+    document.getElementById("unMatch").style.display = "none";
+    let leftValue = document.getElementById("left-text-area").value;
+    let rightValue = document.getElementById("right-text-area").value;
+    if(leftValue==rightValue){
+        document.getElementById("match").style.display = "block";
+    }
+    else{
+        document.getElementById("unMatch").style.display = "block";
+        decrementValue();
+    }
+}
+
+function ClearFields() {
+    document.getElementById("right-text-area").value = "";
+}
+
+function back(){
+    let value = document.getElementById("right-text-area").value;
+    let newValue = value.length -1;
+    document.getElementById("right-text-area").value = value.substring(0,newValue);    
+}
+
+function decrementValue(){
+    let value = document.getElementById("try-value").innerText;
+    value--;
+    document.getElementById("try-value").innerText = value;
+    if(value == 0){
+        document.getElementById("submitBtn").disabled = true;
+    }
 }
